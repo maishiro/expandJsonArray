0eb6a16813f47e944e4c1c96e0ca0b2ca494b542
maishiro <16446436+maishiro@users.noreply.github.com>
2023-02-08 23:00:20 +0900

    展開、変換する"items"がない時、そのまま出力されるようにした

--- main.go
+++ main.go
@@ -31,38 +31,52 @@ func main() {
 
 		c, found := metric.GetField(itemName)
 		if !found {
-			fmt.Fprintf(os.Stderr, "metric has no %s field\n", itemName)
-			os.Exit(1)
-		}
-		switch t := c.(type) {
-		case string:
-			var items []interface{}
-			if err := json.Unmarshal([]byte(t), &items); err != nil {
-				fmt.Fprintf(os.Stderr, "ERR %v\\n", err)
+			b, err := serializer.Serialize(metric)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "ERR %v\n", err)
 				os.Exit(1)
 			}
-			metric.RemoveField(itemName)
-			for _, values := range items {
-				met := metric.Copy()
-				switch vv := values.(type) {
-				case map[string]interface{}:
-					for k, v := range vv {
-						met.AddField(k, v)
+			fmt.Fprint(os.Stdout, string(b))
+		} else {
+			switch t := c.(type) {
+			case string:
+				var items []interface{}
+				if err := json.Unmarshal([]byte(t), &items); err != nil {
+					fmt.Fprintf(os.Stderr, "ERR %v\\n", err)
+					os.Exit(1)
+				}
+				metric.RemoveField(itemName)
+				for _, values := range items {
+					met := metric.Copy()
+					switch vv := values.(type) {
+					case map[string]interface{}:
+						for k, v := range vv {
+							switch v3 := v.(type) {
+							case string:
+							case float64:
+								met.AddField(k, v3)
+							case []interface{}:
+								vs, _ := json.Marshal(v)
+								met.AddField(k, vs)
+							default:
+								fmt.Fprintf(os.Stderr, "unknown type v, it's a %T\n", v)
+							}
+						}
+					default:
+						fmt.Fprintf(os.Stderr, "unknown type, it's a %T\n", values)
+						os.Exit(1)
 					}
-				default:
-					fmt.Fprintf(os.Stderr, "unknown type, it's a %T\n", values)
-					os.Exit(1)
+					b, err := serializer.Serialize(met)
+					if err != nil {
+						fmt.Fprintf(os.Stderr, "ERR %v\n", err)
+						os.Exit(1)
+					}
+					fmt.Fprint(os.Stdout, string(b))
 				}
-				b, err := serializer.Serialize(met)
-				if err != nil {
-					fmt.Fprintf(os.Stderr, "ERR %v\n", err)
-					os.Exit(1)
-				}
-				fmt.Fprint(os.Stdout, string(b))
+			default:
+				fmt.Fprintf(os.Stderr, "%s is not an unknown type, it's a %T\n", itemName, c)
+				os.Exit(1)
 			}
-		default:
-			fmt.Fprintf(os.Stderr, "%s is not an unknown type, it's a %T\n", itemName, c)
-			os.Exit(1)
 		}
 	}
 }
